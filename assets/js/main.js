e436ceb4e73d29f33fbc3e36376d2818ab394dba
K <khayusaki@gmail.com>
2015-03-27 22:17:29 +0800

    Update styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $('body').removeClass('no-js');
 
-  $('a.blog-button').click(function() {
+  $('a.blog-button').click(function(e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
@@ -12,6 +12,7 @@ $(document).ready(function() {
       $('.panel-cover').css('max-width',currentWidth);
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
     }
+    e.preventDefault();
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
